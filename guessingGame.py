51426eec2afef8dcce466f79ef0d61ab4a49be55
Nanakya <71440003+Nanakya@users.noreply.github.com>
2022-01-19 22:20:50 +0530

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,14 @@
+import random
+random=1-9
+number=1-9
+chances=5
+GuessANumberFrom1-9=(input)
+
+while chances < 5:
+
+if guess == number:
+    print("Congratulations you won!")
+    break
+
+if not chances < 5:
+    print("You lose :( The number is" number)
